81ad0500deab330e415257f1bcdd21903cea0f94
Daniel Johansson <johanssondaniel93@gmail.com>
2019-08-05 10:05:56 +0900

    add CANVAS_SIZE, BOARD_SIZE, IMG_NAMES

--- src/shared/Constants.js
+++ src/shared/Constants.js
@@ -1,5 +1,18 @@
 module.exports = Object.freeze ({
+    CANVAS_SIZE: {
+        width: 1000,
+        height: 1000,
+    },
+    BOARD_SIZE: {
+        width: 100,
+        height: 100,
+    },
+    IMG_NAMES: [
+        'and.svg'
+    ],
     SOCKET_MSG: {
         USERNAME_SUBMITTED: 'username_was_submitted',
+        UPDATE: 'server_update',
+        INPUT: 'user_input',
     }
 })
